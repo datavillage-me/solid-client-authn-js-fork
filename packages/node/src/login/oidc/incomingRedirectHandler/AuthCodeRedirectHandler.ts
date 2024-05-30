f8df3a95046d97c4c1869a9804fd3d3d96b5d23a
Michiel Rogissart <michiel@datavillage.me>
2024-05-30 18:07:34 +0200

    test authenticated fetch

--- packages/node/src/login/oidc/incomingRedirectHandler/AuthCodeRedirectHandler.ts
+++ packages/node/src/login/oidc/incomingRedirectHandler/AuthCodeRedirectHandler.ts
@@ -64,7 +64,7 @@ export class AuthCodeRedirectHandler implements IIncomingRedirectHandler {
     private sessionInfoManager: ISessionInfoManager,
     private issuerConfigFetcher: IIssuerConfigFetcher,
     private clientRegistrar: IClientRegistrar,
-    private tokenRefresher: ITokenRefresher,
+    private tokenRefresher: ITokenRefresher
   ) {
     this.storageUtility = storageUtility;
     this.sessionInfoManager = sessionInfoManager;
@@ -82,18 +82,18 @@ export class AuthCodeRedirectHandler implements IIncomingRedirectHandler {
       );
     } catch (e) {
       throw new Error(
-        `[${redirectUrl}] is not a valid URL, and cannot be used as a redirect URL: ${e}`,
+        `[${redirectUrl}] is not a valid URL, and cannot be used as a redirect URL: ${e}`
       );
     }
   }
 
   async handle(
     inputRedirectUrl: string,
-    eventEmitter?: EventEmitter,
+    eventEmitter?: EventEmitter
   ): Promise<IncomingRedirectResult> {
     if (!(await this.canHandle(inputRedirectUrl))) {
       throw new Error(
-        `AuthCodeRedirectHandler cannot handle [${inputRedirectUrl}]: it is missing one of [code, state].`,
+        `AuthCodeRedirectHandler cannot handle [${inputRedirectUrl}]: it is missing one of [code, state].`
       );
     }
 
@@ -103,25 +103,25 @@ export class AuthCodeRedirectHandler implements IIncomingRedirectHandler {
 
     const sessionId = await getSessionIdFromOauthState(
       this.storageUtility,
-      oauthState,
+      oauthState
     );
     if (sessionId === undefined) {
       throw new Error(
-        `No stored session is associated with the state [${oauthState}]`,
+        `No stored session is associated with the state [${oauthState}]`
       );
     }
 
     const oidcContext = await loadOidcContextFromStorage(
       sessionId,
       this.storageUtility,
-      this.issuerConfigFetcher,
+      this.issuerConfigFetcher
     );
 
     const issuer = new Issuer(configToIssuerMetadata(oidcContext.issuerConfig));
     // This should also retrieve the client from storage
     const clientInfo: IClient = await this.clientRegistrar.getClient(
       { sessionId },
-      oidcContext.issuerConfig,
+      oidcContext.issuerConfig
     );
     const client = new issuer.Client({
       client_id: clientInfo.clientId,
@@ -146,7 +146,7 @@ export class AuthCodeRedirectHandler implements IIncomingRedirectHandler {
       // The KeyLike type is dynamically bound to either KeyObject or CryptoKey
       // at runtime depending on the environment. Here, we know we are in a NodeJS
       // context.
-      { DPoP: dpopKey?.privateKey as KeyObject },
+      { DPoP: dpopKey?.privateKey as KeyObject }
     );
 
     if (
@@ -155,7 +155,7 @@ export class AuthCodeRedirectHandler implements IIncomingRedirectHandler {
     ) {
       // The error message is left minimal on purpose not to leak the tokens.
       throw new Error(
-        `The Identity Provider [${issuer.metadata.issuer}] did not return the expected tokens: missing at least one of 'access_token', 'id_token.`,
+        `The Identity Provider [${issuer.metadata.issuer}] did not return the expected tokens: missing at least one of 'access_token', 'id_token.`
       );
     }
     let refreshOptions: RefreshOptions | undefined;
@@ -174,6 +174,17 @@ export class AuthCodeRedirectHandler implements IIncomingRedirectHandler {
       expiresIn: tokenSet.expires_in,
     });
 
+    console.log("!!! Testing authenticated fetch");
+
+    const result = await authFetch(
+      "https://michielrogissart-datavillage.solidcommunity.net/.datavillage/test2.txt",
+      {
+        headers: { Accept: "text/plain" },
+        method: "GET",
+      }
+    );
+    console.log("!!! Authenticated fetch returned ", result.status);
+
     // tokenSet.claims() parses the ID token, validates its signature, and returns
     // its payload as a JSON object.
     const webid = await getWebidFromTokenPayload(
@@ -182,7 +193,7 @@ export class AuthCodeRedirectHandler implements IIncomingRedirectHandler {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       issuer.metadata.jwks_uri!,
       issuer.metadata.issuer,
-      client.metadata.client_id,
+      client.metadata.client_id
     );
 
     await saveSessionInfoToStorage(
@@ -193,13 +204,13 @@ export class AuthCodeRedirectHandler implements IIncomingRedirectHandler {
       tokenSet.access_token,
       tokenSet.refresh_token,
       undefined,
-      dpopKey,
+      dpopKey
     );
 
     const sessionInfo = await this.sessionInfoManager.get(sessionId);
     if (!sessionInfo) {
       throw new Error(
-        `Could not find any session information associated with SessionID [${sessionId}] in our storage.`,
+        `Could not find any session information associated with SessionID [${sessionId}] in our storage.`
       );
     }
 
